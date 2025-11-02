ab1ca055ee48114a328d5d62b8069f497f82467a
aater <aater.ayoub@gmail.com>
2025-11-02 23:40:38 +0100

    fix : fix document annotation

--- my_backend/src/Document/Article.php
+++ my_backend/src/Document/Article.php
@@ -5,43 +5,77 @@ namespace App\Document;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * @ODM\Document(collection="articles")
- */
+#[ODM\Document(collection: "articles")]
 class Article
 {
-    /**
-     * @ODM\Id
-     */
-    private $id;
+    #[ODM\Id]
+    private ?string $id = null;
 
-    /** 
-     * @ODM\Field(type="string") 
-     * @Assert\NotBlank 
-    */
-    private $title;
+    #[ODM\Field(type: "string")]
+    #[Assert\NotBlank]
+    private ?string $title = null;
 
-    /** 
-     * @ODM\Field(type="string") 
-     * @Assert\NotBlank 
-     **/
-    private $content;
+    #[ODM\Field(type: "string")]
+    #[Assert\NotBlank]
+    private ?string $content = null;
 
-    /** @ODM\ReferenceOne(targetDocument=User::class, storeAs="id") */
-    private $author;
+    #[ODM\ReferenceOne(targetDocument: User::class, storeAs: "id")]
+    private ?User $author = null;
 
-    /** @ODM\Field(type="date") */
-    private $publishedAt;
+    #[ODM\Field(type: "date")]
+    private ?\DateTimeInterface $publishedAt = null;
 
-    public function __construct() { $this->publishedAt = new \DateTime(); }
+    public function __construct()
+    {
+        $this->publishedAt = new \DateTime();
+    }
 
-    public function getId(): ?string { return (string) $this->id; }
-    public function getTitle(): ?string { return $this->title; }
-    public function setTitle(string $t): self { $this->title = $t; return $this; }
-    public function getContent(): ?string { return $this->content; }
-    public function setContent(string $c): self { $this->content = $c; return $this; }
-    public function getAuthor(): ?User { return $this->author; }
-    public function setAuthor(User $u): self { $this->author = $u; return $this; }
-    public function getPublishedAt(): ?\DateTimeInterface { return $this->publishedAt; }
-    public function setPublishedAt(\DateTimeInterface $d): self { $this->publishedAt = $d; return $this; }
+    public function getId(): ?string
+    {
+        return (string) $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $t): self
+    {
+        $this->title = $t;
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $c): self
+    {
+        $this->content = $c;
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(User $u): self
+    {
+        $this->author = $u;
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeInterface $d): self
+    {
+        $this->publishedAt = $d;
+        return $this;
+    }
 }
--- my_backend/src/Document/User.php
+++ my_backend/src/Document/User.php
@@ -5,40 +5,63 @@ namespace App\Document;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * @ODM\Document(collection="users")
- */
+#[ODM\Document(collection: "users")]
+#[ODM\Index(keys: ["email" => "asc"], options: ["unique" => true])]
 class User
 {
-    /**
-     * @ODM\Id
-     */
-    private $id;
+    #[ODM\Id]
+    private ?string $id = null;
 
-    /**
-     * @ODM\Field(type="string")
-     * @ODM\Index(unique=true)
-     * @Assert\NotBlank
-     * @Assert\Email
-     */
-    private $email;
+    #[ODM\Field(type: "string")]
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    private ?string $email = null;
 
-    /**
-     * @ODM\Field(type="string")
-     * @Assert\NotBlank
-     */
-    private $name;
+    #[ODM\Field(type: "string")]
+    #[Assert\NotBlank]
+    private ?string $name = null;
 
-    /**
-     * @ODM\Field(type="string")
-     */
-    private $password; // hashed
+    #[ODM\Field(type: "string")]
+    private ?string $password = null; // hashed
 
-    public function getId(): ?string { return (string) $this->id; }
-    public function getEmail(): ?string { return $this->email; }
-    public function setEmail(string $email): self { $this->email = $email; return $this; }
-    public function getName(): ?string { return $this->name; }
-    public function setName(string $name): self { $this->name = $name; return $this; }
-    public function getPassword(): ?string { return $this->password; }
-    public function setPassword(string $pw): self { $this->password = $pw; return $this; }
+    public function getId(): ?string
+    {
+        return (string) $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $pw): self
+    {
+        $this->password = $pw;
+        
+        return $this;
+    }
 }
